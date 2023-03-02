0ab3181cce02a51a484c89c70eb4eaca94ebcb03
shyamtiwari02062001 <shyamtiwari02062001@gmail.com>
2023-03-02 08:30:01 +0530

    Basic

--- App.js
+++ App.js
@@ -1,11 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import MapView, { Marker, Polyline } from "react-native-maps";
+
+import * as Location from "expo-location";
+import pinPoints from "./constants/pinpoints";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        initialRegion={{
+          latitude: 23.3474,
+          longitude: 85.417,
+          latitudeDelta: 0.002,
+          longitudeDelta: 0.002,
+        }}
+        mapType="satellite"
+        style={styles.map}
+      >
+        {pinPoints.map((item, i) => (
+          <Marker
+            key={i}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
+            title={"SBU"}
+            description={item.description}
+          />
+        ))}
+        <Polyline
+          coordinates={[
+            {
+              latitude: 23.35577,
+              longitude: 85.41308,
+            },
+            {
+              latitude: 23.35443,
+              longitude: 85.41305,
+            },
+          ]}
+          strokeColor="#000"
+          strokeColors={["#7F0000"]}
+          strokeWidth={10}
+        />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +75,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
---
+++ constants/pinpoints.js
@@ -0,0 +1,133 @@
+const pinPoints = [
+  {
+    latitude: 23.3474,
+    longitude: 85.417,
+    description: "ADMINISTRATIVE GROUND",
+  },
+  {
+    latitude: 23.3478,
+    longitude: 85.4168,
+    description: "VC SIR HOUSE",
+  },
+  {
+    latitude: 23.3474,
+    longitude: 85.4174,
+    description: "ADMINISTRATIVE BUILDING",
+  },
+  {
+    latitude: 23.3472,
+    longitude: 85.4165,
+    description: "ACADEMIC BUILDING",
+  },
+  {
+    latitude: 23.3475,
+    longitude: 85.4157,
+    description: "NEW PARKING",
+  },
+  {
+    latitude: 23.34834,
+    longitude: 85.415,
+    description: "GIRLS HOSTEL",
+  },
+  {
+    latitude: 23.3485,
+    longitude: 85.414,
+    description: "NEW CANTEEN",
+  },
+  {
+    latitude: 23.3489,
+    longitude: 85.4138,
+    description: "MESS",
+  },
+  {
+    latitude: 23.3488,
+    longitude: 85.4141,
+    description: "DIE MENSA",
+  },
+  {
+    latitude: 23.3497,
+    longitude: 85.4146,
+    description: "NURSING BLOCK",
+  },
+  {
+    latitude: 23.3494,
+    longitude: 85.4148,
+    description: "OLD PARKING",
+  },
+  {
+    latitude: 23.3496,
+    longitude: 85.4138,
+    description: "PLAYGROUND",
+  },
+  {
+    latitude: 23.3503,
+    longitude: 85.414,
+    description: "",
+  },
+  {
+    latitude: 23.3502,
+    longitude: 85.4135,
+    description: "WORKSHOP",
+  },
+  {
+    latitude: 23.35019,
+    longitude: 85.4133,
+    description: "IC",
+  },
+  {
+    latitude: 23.3509,
+    longitude: 85.4135,
+    description: "POND",
+  },
+  {
+    latitude: 23.3532,
+    longitude: 85.41325,
+    description: "AUDITORIUM",
+  },
+  {
+    latitude: 23.35455,
+    longitude: 85.4134,
+    description: "BASKETBALL COURT",
+  },
+  {
+    latitude: 23.3538,
+    longitude: 85.4137,
+    description: "PLAYGROUND",
+  },
+  {
+    latitude: 23.3551,
+    longitude: 85.41355,
+    description: "SARALA BIRLA PUBLIC SCHOOL",
+  },
+  {
+    latitude: 23.3552,
+    longitude: 85.4142,
+    description: "SBPS PARKING",
+  },
+  {
+    latitude: 23.35585,
+    longitude: 85.4139,
+    description: "SBPS MAIN GATE",
+  },
+  {
+    latitude: 23.35577,
+    longitude: 85.41309,
+    description: "GATE NO 1",
+  },
+  {
+    latitude: 23.35011,
+    longitude: 85.41325,
+    description: "GATE NO 7",
+  },
+  {
+    latitude: 23.35443,
+    longitude: 85.41296,
+    description: "GATE NO 5",
+  },
+  {
+    latitude: 23.35443,
+    longitude: 85.4131,
+    description: "GATE NO 4",
+  },
+];
+export default pinPoints;
