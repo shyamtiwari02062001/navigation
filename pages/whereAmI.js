e6ebea23975208c844248af4d7deb6f049872181
shyamtiwari02062001 <shyamtiwari02062001@gmail.com>
2023-03-04 16:19:18 +0530

    Where am i is ready

--- pages/whereAmI.js
+++ pages/whereAmI.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import * as Location from "expo-location";
 import MapView, { Marker } from "react-native-maps";
-
+import pinPoints from "../constants/pinpoints";
+import distance from "../functions/distance";
 const WhereAmI = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [success, setSuccess] = useState(false);
+  const [min, setMin] = useState(100000000);
+  const [place, setPlace] = useState("");
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -20,11 +23,23 @@ const WhereAmI = () => {
         (loc) => {
           setLocation(loc);
           setSuccess(true);
+          for (let i = 0; i < pinPoints.length; i++) {
+            value = distance(
+              pinPoints[i].latitude,
+              pinPoints[i].longitude,
+              loc.coords.latitude,
+              loc.coords.longitude,
+              "N"
+            );
+            if (value < min) {
+              setMin(value);
+              setPlace(pinPoints[i].description);
+            }
+          }
         }
       );
     })();
   }, []);
-
   let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
@@ -33,10 +48,9 @@ const WhereAmI = () => {
   }
   return (
     <View>
-      <Text>{text}</Text>
       {success && (
         <MapView
-          initialRegion={{
+          region={{
             latitude: location.coords.latitude,
             longitude: location.coords.longitude,
             latitudeDelta: 0.002,
@@ -45,16 +59,43 @@ const WhereAmI = () => {
           mapType="satellite"
           style={styles.map}
         >
+          {pinPoints.map((item, i) => (
+            <Marker
+              pinColor="#9e7bb5"
+              key={i}
+              coordinate={{
+                latitude: item.latitude,
+                longitude: item.longitude,
+              }}
+              title={"SBU"}
+              description={item.description}
+            />
+          ))}
           <Marker
-            pinColor="#9e7bb5"
-            key={0}
+            key={100}
+            pinColor="#4B0082"
             coordinate={{
               latitude: location.coords.latitude,
               longitude: location.coords.longitude,
             }}
-            title={"SBU"}
-            description={"your location"}
-          />
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <View style={styles.tooltip}>
+                <Text>{`Nearest to ${place}`}</Text>
+              </View>
+              <View style={styles.TriangleShapeCSS} />
+              <Image
+                source={require("../assets/location.png")}
+                style={styles.img}
+              />
+            </View>
+          </Marker>
         </MapView>
       )}
     </View>
@@ -68,5 +109,26 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  img: {
+    width: 60,
+    height: 60,
+  },
+  tooltip: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 20,
+  },
+  TriangleShapeCSS: {
+    borderLeftWidth: 10,
+    borderRightWidth: 10,
+    borderTopWidth: 10,
+    borderStyle: "solid",
+    backgroundColor: "transparent",
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    borderTopColor: "#fff",
   },
 });
