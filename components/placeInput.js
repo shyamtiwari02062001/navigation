c200b4ae36423c65384f3c0db7f5c05c8aa53b58
shyamtiwari02062001 <shyamtiwari02062001@gmail.com>
2023-03-05 09:33:54 +0530

    Modified the code of path

---
+++ components/placeInput.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { View } from "react-native";
+const PlaceInput = () => {
+  return <View style={{ flex: 1, backgroundColor: "#000" }}></View>;
+};
+export default PlaceInput;
--- pages/path.js
+++ pages/path.js
@@ -1,11 +1,9 @@
-
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import * as ScreenOrientation from "expo-screen-orientation";
 import { useEffect, useState } from "react";
-import PortraitPath from "../components/portraitPath";
-import LandscapePath from "../components/landscapePath";
-export default function Path() {
+import { View, TextInput, StyleSheet, Dimensions } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import pinPoints from "../constants/pinpoints";
+const Path = () => {
   const [orientation, setOrientation] = useState(null);
   useEffect(() => {
     checkOrientation();
@@ -23,17 +21,84 @@ export default function Path() {
   const handleOrientationChange = (o) => {
     setOrientation(o.orientationInfo.orientation);
   };
-  return orientation === 1 || orientation === 2 ? (
-    <PortraitPath />
-  ) : (
-    <LandscapePath />
+  return (
+    <View>
+      <View
+        style={{
+          flexDirection:
+            orientation === 1 || orientation === 2 ? "column" : "row",
+          height:
+            orientation === 1 || orientation === 2
+              ? Dimensions.get("window").height * 0.2
+              : Dimensions.get("window").height * 0.15,
+          ...styles.inputContainer,
+        }}
+      >
+        <TextInput
+          style={{
+            width: orientation === 1 || orientation === 2 ? "90%" : "45%",
+            padding: Dimensions.get("window").height * 0.015,
+            ...styles.input,
+          }}
+        />
+        <TextInput
+          style={{
+            width: orientation === 1 || orientation === 2 ? "90%" : "45%",
+            padding: Dimensions.get("window").height * 0.015,
+            ...styles.input,
+          }}
+        />
+      </View>
+      <MapView
+        initialRegion={{
+          latitude: 23.3474,
+          longitude: 85.417,
+          latitudeDelta: 0.002,
+          longitudeDelta: 0.002,
+        }}
+        mapType="satellite"
+        style={{
+          height:
+            orientation === 1 || orientation === 2
+              ? Dimensions.get("window").height * 0.8
+              : Dimensions.get("window").height * 0.85,
+          ...styles.map,
+        }}
+      >
+        {pinPoints.map((item, i) => (
+          <Marker
+            pinColor="#9e7bb5"
+            key={i}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
+            title={"SBU"}
+            description={item.description}
+          />
+        ))}
+      </MapView>
+    </View>
   );
-}
+};
+export default Path;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  map: {
+    width: "100%",
+  },
+  input: {
+    borderRadius: 20,
+    borderWidth: 4,
+    borderColor: "#4B0082",
+    fontSize: 25,
+    color: "#4B0082",
+  },
+  inputContainer: {
+    backgroundColor: "#9e7bb5",
+    justifyContent: "space-around",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
